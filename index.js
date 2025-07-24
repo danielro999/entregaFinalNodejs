4691061ee4b98f78cb46338a27d3c06a98cd9f45
Daniel <persensus@yahoo.com.ar>
2025-07-24 12:23:44 -0300

    cambio de auth solo para post, delete y put

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import productRousters from "./src/routes/products.router.js";
 import authRouter from "./src/routes/auth.routes.js"
-import { auth } from "./src/middleware/auth.middelware.js";
+
 import raiz from "./src/routes/raiz.routes.js"
 
 const app = express();
@@ -11,10 +11,10 @@ app.use(express.json())
 
 app.use(raiz)
 
-app.use('/api', auth ,productRousters);
+app.use('/api',productRousters);
 
 app.use(authRouter)
 
 app.use((req, res, next)=> res.status(404).json({error: "no found"}))
 
-app.listen(PORT, ()=> console.log("server up!!"))
+app.listen(PORT, ()=> console.log("server up!!"))   
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
+import { auth } from "../middleware/auth.middelware.js";
 import * as productsControllers from "../controllers/products.controller.js";
 
 const router = Router();
 
 router.get('/products',productsControllers.getAllProducts);
 router.get('/products/:id', productsControllers.getProductById)
-router.post('/products/', productsControllers.createProduct)
-router.delete('/products/:id', productsControllers.deleteProduct)
-router.put('/products/:id', productsControllers.updarteProduct)
+router.post('/products/', auth,productsControllers.createProduct)
+router.delete('/products/:id', auth,productsControllers.deleteProduct)
+router.put('/products/:id', auth,productsControllers.updarteProduct)
 
 export default router;
