4141bc0cc8d67ef3371720ad915976c17efa577a
Daniel <persensus@yahoo.com.ar>
2025-07-24 11:41:09 -0300

    agregado el archivo vercel.json

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,98 +1,79 @@
-/* import fs from "fs";
-import path from "path";
-
-const __dirname = import.meta.dirname;
-
-const dataPath = path.join(__dirname, '../data/products.json');
-
-export const getAllProducts = ()=> {
-    const data = fs.readFileSync(dataPath, "utf-8")
-    return JSON.parse(data)
-}
-
-export const getProductById = (id)=>{
-    const products = getAllProducts();
-    const product = products.find(p => p.id == id);
-    return product
-}  */
-import { db } from '../data/data.js';
-
+import { db } from "../data/data.js";
 import {
-    collection, 
-    getDoc,
-    getDocs,
-    addDoc,
-    deleteDoc,
-    doc,
-    enableNetwork,
-    setDoc
-} from 'firebase/firestore';
+  collection,
+  getDoc,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  enableNetwork,
+  setDoc,
+} from "firebase/firestore";
 
-const productsCollection = collection(db, 'productos');
+const productsCollection = collection(db, "productos");
 
-export async function getAllProducts(){
-    const querySnapshot = await getDocs(productsCollection);
-   // console.log(querySnapshot)
-    const products = [];
-    querySnapshot.forEach((doc)=>products.push({id:doc.id, ...doc.data()}))
+export async function getAllProducts() {
+  const querySnapshot = await getDocs(productsCollection);
+  // console.log(querySnapshot)
+  const products = [];
+  querySnapshot.forEach((doc) => products.push({ id: doc.id, ...doc.data() }));
   /*   {        
         console.log(doc.data() + doc.id)
     }) */
-    console.log( products)
-    return products;
+  console.log(products);
+  return products;
 }
 
-export const getProductById = async (id)=>{
-    try{
-        const productRef = doc(productsCollection, id)
-        const productDoc = await getDoc(productRef)
-        if(productDoc.exists){
-            return productDoc.data() // el objeto sin el id, con id => { productDoc.id, product.data()}
-        } else {
-            return null
-        }
-    } catch(error){
-        console.log(error);
+export const getProductById = async (id) => {
+  try {
+    const productRef = doc(productsCollection, id);
+    const productDoc = await getDoc(productRef);
+    if (productDoc.exists) {
+      return productDoc.data(); // el objeto sin el id, con id => { productDoc.id, product.data()}
+    } else {
+      return null;
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const createProductModel = async (product) =>{
-    try{
-        await addDoc(productsCollection,product)
-        return product
-    } catch(error) {
-        console.log(error )
-    }
-}
+export const createProductModel = async (product) => {
+  try {
+    await addDoc(productsCollection, product);
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deleteProduct = async (id) => {
-      try{
-        const productRef = doc(productsCollection, id)
-        const productDoc = await getDoc(productRef)
-        if(productDoc.exists){
-            await deleteDoc(productRef)
-            return productDoc.data() // el objeto sin el id, con id => { productDoc.id, product.data()}
-        } else {
-            return null
-        }
-    } catch(error){
-        console.log(error);
+  try {
+    const productRef = doc(productsCollection, id);
+    const productDoc = await getDoc(productRef);
+    if (productDoc.exists) {
+      await deleteDoc(productRef);
+      return productDoc.data(); // el objeto sin el id, con id => { productDoc.id, product.data()}
+    } else {
+      return null;
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //put
- export const updarteProduct = async (id, product) => {
-    try{
-        const productRef = doc(productsCollection, id)
-        const productDoc = await getDoc(productRef)
-        if(productDoc.exists){
-            await setDoc(productRef, product)
-        return {id, ...product}
-        }else{
-            return null
-        }
-    } catch(error){
-        console.log(error);
+export const updarteProduct = async (id, product) => {
+  try {
+    const productRef = doc(productsCollection, id);
+    const productDoc = await getDoc(productRef);
+    if (productDoc.exists) {
+      await setDoc(productRef, product);
+      return { id, ...product };
+    } else {
+      return null;
     }
-
- }
+  } catch (error) {
+    console.log(error);
+  }
+};
